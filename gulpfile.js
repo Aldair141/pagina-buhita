23187628f6fa3e152e7ce7661e92197c4680fdc6
Juan Aldair <aldair.rc70@gmail.com>
2021-04-25 23:35:42 -0500

    Empezando a trabajar con la tienda - 25042021

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,28 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-function css() {
-    return gulp.src('scss/estilos.scss')
+function css_libros() {
+    return gulp.src('scss/libros_gratis/libros_gratis.scss')
+        .pipe(autoprefixer())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('assets/css'));
+}
+
+function css_tienda() {
+    return gulp.src('scss/tienda/tienda.scss')
         .pipe(autoprefixer())
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('assets/css'));
 }
 
 function watchFiles() {
-    gulp.watch('scss/*.scss', css);
+    gulp.watch('scss/shared/*.scss', css_libros);
+    gulp.watch('scss/libros_gratis/*.scss', css_libros);
+    gulp.watch('scss/*.scss', css_libros);
+
+    gulp.watch('scss/shared/*.scss', css_tienda);
+    gulp.watch('scss/tienda/*.scss', css_tienda);
+    gulp.watch('scss/*.scss', css_tienda);
 }
 
 gulp.task('watch', gulp.parallel(watchFiles));
