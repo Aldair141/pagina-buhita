ddcd338016bc58749a9e42617185c0e167f90a5f
Juan Aldair <aldair.rc70@gmail.com>
2021-04-27 00:15:07 -0500

    Implementación plantilla detalle del libro - 27042021

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,23 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-function css_libros() {
-    return gulp.src('scss/libros_gratis/libros_gratis.scss')
+function compilar(ruta) {
+    return gulp.src(ruta)
         .pipe(autoprefixer())
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('assets/css'));
 }
 
+function css_libros() {
+    return compilar('scss/libros_gratis/libros_gratis.scss');
+}
+
 function css_tienda() {
-    return gulp.src('scss/tienda/tienda.scss')
-        .pipe(autoprefixer())
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('assets/css'));
+    return compilar('scss/tienda/tienda.scss');
+}
+
+function css_detalle() {
+    return compilar('scss/detalle/detalle-libro.scss');
 }
 
 function watchFiles() {
@@ -24,6 +29,10 @@ function watchFiles() {
     gulp.watch('scss/shared/*.scss', css_tienda);
     gulp.watch('scss/tienda/*.scss', css_tienda);
     gulp.watch('scss/*.scss', css_tienda);
+
+    gulp.watch('scss/shared/*.scss', css_detalle);
+    gulp.watch('scss/detalle/*.scss', css_detalle);
+    gulp.watch('scss/*.scss', css_detalle);
 }
 
 gulp.task('watch', gulp.parallel(watchFiles));
